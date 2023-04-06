1a5bfa5e21a8b2880c277beb718cd04ad2c47af1
Aldijana Čulezović <116015661+aldijanaa@users.noreply.github.com>
2023-04-06 21:19:06 +0200

    Update execle.c

--- execle.c
+++ execle.c
@@ -1,28 +1,35 @@
 
 //Task 1.3.2.1. --> Intermediate: execle()  
-//Executing a binary file "ls" located in /bin directory
-//New process is created using execle(), the env variables passed to the function is used to initialize the 'environ' array in the new process.
-// If the env argument is NULL, the new process inherits the environment variables from the parent process.
-//this code will print out if execle succeds(), all current working directory (command ls)
+//execle() used to execute 'env' command which will print out the  environment variables in the current shell 
+
+//execle() is one of functions in the exec family used to execute new program within the process
+//when exec function is called, current process is replaced with the new program specified by function call
+ 
 //To compile, type gcc -o fileName fileName.c -Wall
 //To run, type ./fileName
 
- 
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h>  //for system calls
 
 int main() {
-    char *env[] = {"MY_VAR=hello", NULL}; // environment variable to pass
-    char *args[] = {"env", NULL}; // program and its arguments
+    char *env[] = {"MY_VAR=hello", NULL}; // environment variable 
+    char *args[] = {"env", NULL}; // Command and its arguments
 
-    execle("/usr/bin/env", args[0], NULL, env); // calling execle function
+    //calling execle() function
+    execle("/usr/bin/env", args[0], NULL, env);  //each of these commands is located in "bin" directory
 
-    // This line won't be printed if execle() succeeds
-    printf("This line won't be printed because execle() replaced the current process.\n");
+    // if execle() succeeds - this line won't be printed 
+    printf("This line won't be printed because execle() replaced the current process.Goodbye\n");
 
     // If execle() fails
     perror("execle() failed");
     exit(1);
+    
+    // Retrieve and print the value of the MY_VAR variable
+    char *my_var = getenv("MY_VAR");  
+    printf("The value of MY_VAR is: %s\n", my_var);
 }
+
 

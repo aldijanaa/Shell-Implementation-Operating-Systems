6b94eef804ffa166b414fea0859d558f58e83800
Aldijana Čulezović <116015661+aldijanaa@users.noreply.github.com>
2023-04-06 21:27:35 +0200

    Update forkwait.c

--- forkwait.c
+++ forkwait.c
@@ -1,6 +1,11 @@
-//Task 1.3.1. 1. & 2. --> Basic : Implementation of fork and wait together
-//To compile, type gcc -o fileName fileName.c -Wall
+//Task 1.3.1. 1. & 2. --> Basic : Implementation of fork(), wait(), and exec() system calls 
+//To compile, type gcc -o fileName fileName.c -Wall  or gcc filename.c -o filename
 //To run, type ./fileName
+//fork() creates new child process
+//wait() parent process waits for child to finish its execution
+//exec() is used to replace current process image with new process image 
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,7 +26,9 @@ int main() {
     }
     else if (child == 0)  //New child process created
     {
-     
+         //When the child process is created, ls will be executed to list contents of current directory
+        char *args[] = {"ls", "-l", NULL};
+        execvp(args[0], args); //Child process is now replaced, and new command starts executing
         printf("Hello, I am child and this is my id: %d\n", (int)getpid());
     }
     else

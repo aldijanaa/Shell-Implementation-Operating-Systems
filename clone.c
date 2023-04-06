648861062ffc7fcf7bbba813ea698434a228c63e
Aldijana Čulezović <116015661+aldijanaa@users.noreply.github.com>
2023-04-06 21:42:56 +0200

    Update clone.c

--- clone.c
+++ clone.c
@@ -3,16 +3,17 @@
 //creates a new ("child") process, in a manner similar to fork. However, the difference is clone() function is used to run a particular function in a separate thread other than the calling //process. 
 //Unlike fork() function where rest of the code after fork() function call will execute both parent and child processes. 
 //But with clone() , a particular function will be executed in //separate thread.
-//To compile, type gcc -o fileName fileName.c -Wall
-//To run, type ./fileName
+
 //When CLONE_VM is set, the calling process and the child process run in the same memory space.
 //When CLONE_FS is set, the caller and the child process share the same filesystem information.
 //When CLONE_FILES is set, the calling process and the child process share the same file descriptor table.
 //When CLONE_SIGHAND is set, the calling process and the child process share the same table of signal handlers.
 //If CLONE_THREAD is set, the child is placed in the same thread group as the calling process. 
 
+//To compile, type gcc -o fileName fileName.c -Wall or gcc fileName.c -o fileName
+//To run, type ./fileName
 
-#define _GNU_SOURCE
+#define _GNU_SOURCE  //non-standard flag for additional functions and macros that are not part of standard C libary
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
@@ -25,7 +26,9 @@ int child_func(void *arg) {
 
 int main() {
     char *stack = malloc(4096);
-    int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD;
+    
+    //Flags specifying child thread should share virtual memory space, file system info, open file descriptors, signal handlers and thread group ID of parent
+    int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD; 
     int ret = clone(child_func, stack + 4096, flags, NULL);
     if (ret == -1) {
         perror("clone failed");

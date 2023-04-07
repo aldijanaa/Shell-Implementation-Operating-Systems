7caf7f420bfb0320fb789991777825441355dc9e
Aldijana Čulezović <116015661+aldijanaa@users.noreply.github.com>
2023-04-07 18:06:33 +0200

    Update forkbomb.c

--- forkbomb.c
+++ forkbomb.c
@@ -37,3 +37,22 @@ int main()
         printf("Not valid input.\n");
     }
 }
+
+
+
+/*Forkbomb can also be presented through this symbol :(){ :|: & };:
+
+: -> This defines a function named : (colon). The function does not take any arguments.
+(){ ... } -> This is the body of the function. It contains a command to call the function recursively, creating an infinite loop. Curly braces create a code block in which the function is performed.
+:-> This calls the function, starting the infinite loop.
+|-> This is the symbol for piping, which connects the output of one command to the input of another.
+:-> This is the name of the function being piped to. The : function is called with its output as input.
+& -> This puts the command in the background, allowing other commands to be executed in the foreground.
+; -> This separates the two commands, allowing them to be executed sequentially.
+: -> This is the final call to the : function, which starts another iteration of the infinite loop.
+
+The combination of these commands creates a chain reaction of process creation, as each call to the function creates two new processes 
+(one to execute the function and one to pipe its output), which in turn create more processes, 
+quickly consuming all available system resources and causing the system to crash or become unresponsive.*/
+
+
